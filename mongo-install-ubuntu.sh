811bd19ef31c3752eb884f97dc15f252b1dc2566
Amol Khanorkar <amol.khanorkar@MOTIFWORKS.IN>
2018-03-10 17:16:35 +0530

    fixed multiple extension issue

--- mongo-install-ubuntu.sh
+++ mongo-install-ubuntu.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Configure mongodb.list file with the correct location
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
@@ -19,3 +21,17 @@ sudo apt-get install -y mongodb-org
 # Uncomment this to bind to all ip addresses
 # sudo sed -i -e 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/g' /etc/mongod.conf
 sudo service mongod restart
+
+sudo mkdir -p /data/db
+
+nohup /usr/bin/mongod  --configsvr --replSet rsconfig1 --dbpath /data/db --bind_ip 0.0.0.0 >> /var/log/mongodb/mongod.log &
+
+cfg="{
+    _id: 'rsconfig1',
+    configsvr: true,
+    members: [
+        {_id: 0, host: '10.0.0.9:27019'},
+        {_id: 1, host: '10.0.0.8:27019'}
+    ]
+}"
+mongo 10.0.0.8:27019 --eval "JSON.stringify(db.adminCommand({'replSetInitiate' : $cfg}))"
