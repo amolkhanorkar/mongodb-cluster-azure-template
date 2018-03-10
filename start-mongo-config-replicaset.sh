3c96241120739c5ff4d85f15395d55be309894bf
Amol Khanorkar <amol.khanorkar@MOTIFWORKS.IN>
2018-03-10 18:23:46 +0530

    fixed multiple extension by handler issue

--- start-mongo-config-replicaset.sh
+++ start-mongo-config-replicaset.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Configure mongodb.list file with the correct location
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
@@ -32,4 +34,4 @@ cfg="{
         {_id: 1, host: '10.0.0.8:27019'}
     ]
 }"
-mongo 10.0.0.8:27019 --eval "JSON.stringify(db.adminCommand({'replSetInitiate' : $cfg}))"
+/usr/bin/mongo 10.0.0.8:27019 --eval "JSON.stringify(db.adminCommand({'replSetInitiate' : $cfg}))"
