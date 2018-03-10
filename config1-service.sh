0560f451fa3eca79ed7de2a87cc6d746ca71e16c
Amol Khanorkar <amol.khanorkar@MOTIFWORKS.IN>
2018-03-10 17:54:47 +0530

    fixed multiple extension by handler issue

--- config1-service.sh
+++ config1-service.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
-sudo mkdir -p /data/db
+# Configure mongodb.list file with the correct location
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 
+# Disable THP
+sudo echo never > /sys/kernel/mm/transparent_hugepage/enabled
+sudo echo never > /sys/kernel/mm/transparent_hugepage/defrag
+sudo grep -q -F 'transparent_hugepage=never' /etc/default/grub || echo 'transparent_hugepage=never' >> /etc/default/grub
+
+# Install updates
+sudo apt-get -y update
+
+# Modified tcp keepalive according to https://docs.mongodb.org/ecosystem/platforms/windows-azure/
+sudo bash -c "sudo echo net.ipv4.tcp_keepalive_time = 120 >> /etc/sysctl.conf"
+
+#Install Mongo DB
+sudo apt-get install -y mongodb-org
+
+# Uncomment this to bind to all ip addresses
+# sudo sed -i -e 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/g' /etc/mongod.conf
+sudo service mongod restart
+
+sudo mkdir -p /data/db
 nohup /usr/bin/mongod  --configsvr --replSet rsconfig1 --dbpath /data/db --bind_ip 0.0.0.0 >> /var/log/mongodb/mongod.log &
